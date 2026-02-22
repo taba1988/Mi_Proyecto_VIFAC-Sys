f69187cb97994cf2aafcb9b584fe0d47bd91330d
taba1988 <orlanduvalie_tabares@soy.sena.edu.co>
2026-02-22 03:13:32 -0500

    Cambios realizados en puerta para Mailsender

--- Backend_VIFAC_Sys/src/java/com/vifac/sys/util/MailSender.java
+++ Backend_VIFAC_Sys/src/java/com/vifac/sys/util/MailSender.java
@@ -64,11 +64,12 @@ public class MailSender {
         // Configuración del servidor SMTP de Gmail
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.port", "587"); 
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.ssl.enable", "true");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2"); 
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 
         // Crear sesión autenticada con las credenciales
         Session session = Session.getInstance(props, new Authenticator() {
