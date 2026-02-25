9724260b335a1e499ef57ab0a4100535eea18a0f
taba1988 <orlanduvalie_tabares@soy.sena.edu.co>
2026-02-25 01:02:40 -0500

    Correcciòn de estilos CSS para el carrusel

--- Backend_VIFAC_Sys/test/com/vifac/sys/test/VerificadorHash.java
+++ Backend_VIFAC_Sys/test/com/vifac/sys/test/VerificadorHash.java
@@ -4,8 +4,8 @@ import org.mindrot.jbcrypt.BCrypt;
 
 public class VerificadorHash {
     public static void main(String[] args) {
-        String passwordPlano = "Familiat14$&"; // La contraseña que quieres verificar
-        String hashBD = "$2a$10$b9ytcokhlAaAFULqonKWkOVDJOzzWRNuAawzuJUn7s5LIkESQSOvK"; // El hash guardado en la BD
+        String passwordPlano = "Sagitario1988$%"; // La contraseña que quieres verificar
+        String hashBD = "$2a$10$eVTCDvnGnjvDo/yIdIWJteYN2cdFDak4emB4FVcBKSjHX2LmwjiHq"; // El hash guardado en la BD
 
         if (BCrypt.checkpw(passwordPlano, hashBD)) {
             System.out.println("La contraseña es correcta");
